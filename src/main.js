b7a3fdd0e6dda2104d0340f886e291d0e7646971
Nontrust <deed1515@naver.com>
2024-01-26 15:10:10 +0900

    [set] globalic momentjs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import router from '@/route'
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import moment from "moment";
 
-createApp(App)
-    .use(router)
+const app = createApp(App)
+
+// set global properties
+app.config.globalProperties.$moment = moment
+
+app.use(router)
     .mount('#app')
+
+/*
+// error handling
+app.config.errorHandler = (err) =>{
+    //error handling
+}
+*/
+
+/*
+// multi app
+const app = createApp(App)
+    .use(router)
+    .mount('#app2');
+*/
