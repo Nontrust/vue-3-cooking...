82cca44ecae3d146431f72adac1968b9ee1be1ce
Nontrust <deed1515@naver.com>
2024-01-26 16:24:07 +0900

    [1_syntax] learn template syntax

--- src/page/essentials/TemplateSyntax.vue
+++ src/page/essentials/TemplateSyntax.vue
@@ -1,13 +1,15 @@
 <script setup>
- import  {ref} from 'vue'
- const rawHtml = ref('<div style="color:red" >이것은 빨간 텍스티입니다.</div>');
- const bindNowDate = ref(Date.now());
+
+ const rawHtml = '<div style="color:red" >이것은 빨간 텍스트입니다.</div>';
+ const bindNowDate = Date.now();
  const multipleAttribute = {
      id: '1',
      class: 2,
      remove: false
  }
-
+ const dynamicAttribute = 'href'
+ const url = "naver.com"
+ const congratulation = (msg) => alert(msg);
 </script>
 
 <template>
@@ -24,14 +26,23 @@
     </div>
     <h2>javascript expression</h2>
     <div>
-        <p>하나의 표현식은 가능: {{Math.random() + 1 }}</p>
+        <strong>하나의 표현식은 가능: {{Math.random() + 1 }}</strong>
         <p>선언형으로 하려고 하면 안됨 </p>
         <!--<p>{{ const a = 1 }}</p>-->
+        <strong>app설정시 globalProperties 설정 후 접근가능</strong>
         <p>window 등의 전역 속성은 표현식에서 접근 불가,</p>
-        <p>app설정시 globalProperties 설정해야 접근가능</p>
+        <p>app config로 정의된 모먼트 {{$moment.now()}}</p>
         <!--<p>{{window.location.href}}</p>-->
-
     </div>
+
+    <h2>Directives</h2>
+    <p v-if="$moment().day()%6 >= 0">this text on weekend</p>
+    <p v-else>see on hollyday</p>
+    <h2>Dynamic Attribute</h2>
+    <a :[dynamicAttribute]='url'> naver.com </a>
+
+    <h2>Event</h2>
+    <button @click="congratulation('have a good day')"> it`s my birth Day</button>
 
 </template>
 
